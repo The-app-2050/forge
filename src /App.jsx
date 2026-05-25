394b6eb53e16ace12daa66561c3b6aa42e4edcc8
jaredbridges90-eng <jaredbridges90@gmail.com>
2026-05-25 04:44:13 -0400

    Update App.jsx

--- src /App.jsx
+++ src /App.jsx
@@ -1,34 +1,27 @@
-iimport React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import ForgotPassword from "./pages/ForgotPassword";
-import ResetPassword from "./pages/ResetPassword";
-import Onboarding from "./pages/Onboarding";
 import Boot from "./pages/Boot";
 import Brief from "./pages/Brief";
 import Recall from "./pages/Recall";
 import ActivityLog from "./pages/ActivityLog";
 import Security from "./pages/Security";
 import Settings from "./pages/Settings";
-import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/boot" />} />
+        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/boot" element={<ProtectedRoute><Boot /></ProtectedRoute>} />
-        <Route path="/brief" element={<ProtectedRoute><Brief /></ProtectedRoute>} />
-        <Route path="/recall" element={<ProtectedRoute><Recall /></ProtectedRoute>} />
-        <Route path="/activity" element={<ProtectedRoute><ActivityLog /></ProtectedRoute>} />
-        <Route path="/security" element={<ProtectedRoute><Security /></ProtectedRoute>} />
-        <Route path="/settings" element={<ProtectedRoute><Settings /></ProtectedRoute>} />
-        <Route path="/onboarding" element={<Onboarding />} />
+        <Route path="/boot" element={<Boot />} />
+        <Route path="/brief" element={<Brief />} />
+        <Route path="/recall" element={<Recall />} />
+        <Route path="/activity" element={<ActivityLog />} />
+        <Route path="/security" element={<Security />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
     </Router>
   );
