9eda8184e9790c3c375702ad6d68c2288aed76b7
jaredbridges90-eng <jaredbridges90@gmail.com>
2026-05-25 04:26:56 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<div>App is loading...</div>} />
+      </Routes>
+    </Router>
+  );
+}
