7326ba8bf1e0cfa3920076f9b7701c4c2fa4337a
jaredbridges90-eng <jaredbridges90@gmail.com>
2026-05-25 06:11:31 -0400

    Update App.jsx

--- src /App.jsx
+++ src /App.jsx
@@ -1,28 +1,35 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Boot from "./pages/Boot";
-import Brief from "./pages/Brief";
-import Recall from "./pages/Recall";
-import ActivityLog from "./pages/ActivityLog";
-import Security from "./pages/Security";
-import Settings from "./pages/Settings";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import ProtectedRoute from './components/ProtectedRoute'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Onboarding from './pages/Onboarding'
+import Boot from './pages/Boot'
+import Brief from './pages/Brief'
+import Recall from './pages/Recall'
+import ActivityLog from './pages/ActivityLog'
+import ForgotPassword from './pages/ForgotPassword'
+import ResetPassword from './pages/ResetPassword'
+import Security from './pages/Security'
+import Settings from './pages/Settings'
+import PageNotFound from './lib/PageNotFound'
 
 export default function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/boot" element={<Boot />} />
-        <Route path="/brief" element={<Brief />} />
-        <Route path="/recall" element={<Recall />} />
-        <Route path="/activity" element={<ActivityLog />} />
-        <Route path="/security" element={<Security />} />
-        <Route path="/settings" element={<Settings />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/" element={<ProtectedRoute><Boot /></ProtectedRoute>} />
+        <Route path="/onboarding" element={<ProtectedRoute><Onboarding /></ProtectedRoute>} />
+        <Route path="/brief" element={<ProtectedRoute><Brief /></ProtectedRoute>} />
+        <Route path="/recall" element={<ProtectedRoute><Recall /></ProtectedRoute>} />
+        <Route path="/activity-log" element={<ProtectedRoute><ActivityLog /></ProtectedRoute>} />
+        <Route path="/security" element={<ProtectedRoute><Security /></ProtectedRoute>} />
+        <Route path="/settings" element={<ProtectedRoute><Settings /></ProtectedRoute>} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
-    </Router>
-  );
+    </BrowserRouter>
+  )
 }
